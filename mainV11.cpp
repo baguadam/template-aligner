b739fdfd2e6d975da583c5db9f051a36dff404e1
baguadam <baguadam@gmail.com>
2023-11-21 12:34:18 +0100

    Update README.md

--- mainV11.cpp
+++ mainV11.cpp
@@ -1,15 +1,3 @@
-// Az aligner osztály template, aminek a template paramétere meghatározza,
-// hogy milyen elemeket tartalmazó vectorok igazításáért felel a típus.
-// Maga az aligner nem tudja, hogy az eligazítás mit is jelent, ezért ezt az
-// altípusokra fogja bízni: center_aligner-rel  középre, a left_aligner-rel
-// balra, a right_aligner-rel jobbra lehet igazítani az aligner-hez adott
-// vectorokat. Az align-nak ténylegesen azokkal a vektorokkal kell dolgoznia,
-// amiket hozzáadtunk, azokért fog felelni. A count művelete megadja, hogy hány
-// vektort érint az igazítás. Az igazítások után az összes vector mérete
-// megegyezik, alapértelmezett a template paraméter default konstruáltjával tölti
-// fel a vektorokat megfelelően, illetve létezik a set_filler metódus, amivel ez
-// átállítható. Az eligazítás a leghosszabb vektorhoz igazodik.
-
 #include <iostream>
 #include <numeric>
 #include <iterator>
